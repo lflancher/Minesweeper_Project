b8ff9b7befbacb9385d6632014a33b8fef678160
lflancher <lflancher@gmail.com>
2024-09-19 18:31:26 -0400

    Add project files.

---
+++ InClassChallenge4.5/CellClickEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassChallenge4._5
+{
+    public class CellClickEventArgs: EventArgs
+    {
+        int col;
+        int row;
+
+        public CellClickEventArgs()
+        {
+
+        }
+
+        public CellClickEventArgs(int col, int row)
+        {
+            Col = col;
+            Row = row;
+        }
+
+        public int Col { get => col; set => col = value; }
+        public int Row { get => row; set => row = value; }
+    }
+}
